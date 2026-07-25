855bdcb7e950989b06188af541f459d03c93cce1
Jeyhun321 <alekberov.ceyhun2002@gmail.com>
2026-07-25 22:34:41 +0400

    Set showcase interval to two seconds

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ import { useI18n } from '../i18n/I18nContext.jsx'
 import { IconArrow } from './Icons.jsx'
 import ProductImage from './ProductImage.jsx'
 
-const AUTOPLAY_MS = 1000
+const AUTOPLAY_MS = 2000
 
 export default function Intro() {
   const { t } = useI18n()
